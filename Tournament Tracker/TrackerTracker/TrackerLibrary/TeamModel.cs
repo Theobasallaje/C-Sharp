dd9a52ba0ed961f11fd517212de6ae85c2a80637
Theobasallaje <theobasallaje@gmail.com>
2020-01-08 11:54:09 -0600

    Class Library Creation

--- Tournament Tracker/TrackerTracker/TrackerLibrary/TeamModel.cs
+++ Tournament Tracker/TrackerTracker/TrackerLibrary/TeamModel.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace TrackerLibrary
-{
-    class TeamModel
-    {
+namespace TrackerLibrary {
+    public class TeamModel {
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+        public string TeamName { get; set; }
     }
 }
