c305c48ff66c4fbb8a1607acaa4efe7df8833211
Theobasallaje <theobasallaje@gmail.com>
2020-01-08 11:56:47 -0600

    MatchupModel Class Library Creation

--- Tournament Tracker/TrackerTracker/TrackerLibrary/MatchupModel.cs
+++ Tournament Tracker/TrackerTracker/TrackerLibrary/MatchupModel.cs
@@ -7,5 +7,6 @@ namespace TrackerLibrary {
         public List<MatchupEntryModel> Entries { get; set; } = new List<MatchupEntryModel>();
         public TeamModel Winner { get; set; }
         public int MatchupRound { get; set; }
+
     }
 }
